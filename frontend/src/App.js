217e8a1952abf4bd8b9ca43feff7bbd59a5d16f5
enbre <noahbrezel@gmail.com>
2021-11-16 10:52:13 -0800

    fixes additional routing issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,6 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import Wishlist from "./pages/Wishlist";
 import Footer from "./components/Footer";
-import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
