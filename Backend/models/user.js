f9ec2384c28ec0d796075c7dc89457fb5952dbaf
Aakaash Meduri <aakaash.meduri@gmail.com>
2021-11-16 17:41:43 -0600

    updates user model to include list of favorite items

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -7,6 +7,7 @@ const userSchema = new mongoose.Schema({
   username: {type: String, required: true, lowercase: true, unique: true},
   email: {type: String, required: true, lowercase: true, unique: true},
   password: String,
+  favorites: {type: Array, required: false},
 }, {
   timestamps: true
 });
