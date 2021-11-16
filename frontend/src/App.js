98b2447d448847ad1c8c7abcea9868bd0bf8b06b
Trace White <traceannwhite@gmail.com>
2021-11-16 12:13:01 -0600

    readded route and switch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
-
 import "./styles/CSS/App.css";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Wishlist from "./pages/Wishlist";
 import Footer from "./components/Footer";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
